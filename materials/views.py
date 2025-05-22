8684ac53a13ee64c8645fa6548c2b0137d7a7099
NataliaEpifanova <epif97@mail.ru>
2025-05-22 13:02:30 +0300

    fixes

--- materials/views.py
+++ materials/views.py
@@ -16,21 +16,26 @@ class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonRetrieveApiView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
